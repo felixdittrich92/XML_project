6114f1417b0ea321473a9acfc04bad387b0c649b
Felix <you@example.com>
2019-12-04 18:47:39 +0100

    Step csv and xml

---
+++ csv_to_xml.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from dicttoxml import dicttoxml
+
+data = pd.read_csv('data/weather.csv', sep='\s+')
+df = pd.DataFrame(data, columns=data.keys())
+
+data_dict = df.to_dict(orient="records")
+xml_data = dicttoxml(data_dict).decode()
+with open("output.xml", "w+") as f:
+    f.write(xml_data)
